a30c0d720e623982a82604ee4155517bcac7d2ee
EnormousApplePie <marynmats@gmail.com>
2021-07-28 18:14:27 +0200

    Removed unused files

--- DLC/LEKMOD/CvGameCore_Expansion2/NQMP/religion/enhancers/DeusVult.sql
+++
@@ -1,2 +0,0 @@
--- Insert SQL Rules Here 
-ALTER TABLE Beliefs ADD DeusVult BOOLEAN DEFAULT false;
--- DLC/LEKMOD/CvGameCore_Expansion2/NQMP/religion/enhancers/PapalPrimacy.sql
+++
@@ -1,2 +0,0 @@
--- Insert SQL Rules Here 
-ALTER TABLE Beliefs ADD FaithPerCityStateThisReligion INTEGER DEFAULT 0;
