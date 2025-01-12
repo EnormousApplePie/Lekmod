7836a542dcfb2b3126f5f9165cdb7c17f2110bfe
ImmoSS <83536444+ImmoSS@users.noreply.github.com>
2025-01-12 20:33:17 +0300

    Create FrontEnd.lua

---
+++ LEKMOD/Lua/UI/FrontEnd.lua
@@ -0,0 +1,23 @@
+-------------------------------------------------
+-- FrontEnd
+-------------------------------------------------
+
+function ShowHideHandler( bIsHide, bIsInit )
+
+		-- Check for game invites first.  If we have a game invite, we will have flipped 
+		-- the Civ5App::eHasShownLegal and not show the legal/touch screens.
+		UI:CheckForCommandLineInvitation();
+		
+    if( not UI:HasShownLegal() ) then
+        UIManager:QueuePopup( Controls.LegalScreen, PopupPriority.LegalScreen );
+    end
+
+    if( not bIsHide ) then
+        Controls.AtlasLogo:SetTexture( "CivilzationVAtlas.dds" );
+    	UIManager:SetUICursor( 0 );
+        UIManager:QueuePopup( Controls.MainMenu, PopupPriority.MainMenu );
+    else
+        Controls.AtlasLogo:UnloadTexture();
+    end
+end
+ContextPtr:SetShowHideHandler( ShowHideHandler );
